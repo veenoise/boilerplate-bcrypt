b52609cdc9e787be334987b468bf847beeffdfa9
veenoise <william.eduard123@gmail.com>
2024-10-02 01:30:57 +0800

    learned how to hash passwords with bcrypt

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 const express     = require('express');
+const bcrypt = require('bcrypt');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
@@ -10,13 +11,19 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res); //true
+    });
+});
 //END_ASYNC
 
 //START_SYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash)
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result)
 
 
 //END_SYNC
